3aebbba4359043293b3189cc82aaf45291871438
norellanac <1005alexis@gmail.com>
2022-02-01 18:17:02 -0600

    implemented laravel passport and cors,  also implemented a resource to return json data from a model object or collection

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/* 
+     fetch('http://localhost:8000/api/v1/users')
+    .then(response => response.json())
+    .then(json => console.log(json)) */
+
+
+
+// ...
+
+Route::apiResource('/v1/users', UserController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
